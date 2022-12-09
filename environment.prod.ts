2c4d39a6f73851c0ea2edd1ab9d617e609052e1e
enriqueJrBd <40614288+enriqueJrBd@users.noreply.github.com>
2022-12-09 12:52:19 -0300

    Add files via upload

---
+++ environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDJyBVPArI0i62wDVdAHkcwVSZo1ZFdzwg",
+    authDomain: "reciclappy-286a2.firebaseapp.com",
+    projectId: "reciclappy-286a2",
+    storageBucket: "reciclappy-286a2.appspot.com",
+    messagingSenderId: "401792967637",
+    appId: "1:401792967637:web:40b528a6aebf80f8b1135c",
+    measurementId: "G-726FH12QPB"
+  }
+  };
+
---
+++ environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyDJyBVPArI0i62wDVdAHkcwVSZo1ZFdzwg",
+  authDomain: "reciclappy-286a2.firebaseapp.com",
+  projectId: "reciclappy-286a2",
+  storageBucket: "reciclappy-286a2.appspot.com",
+  messagingSenderId: "401792967637",
+  appId: "1:401792967637:web:40b528a6aebf80f8b1135c",
+  measurementId: "G-726FH12QPB"
+}
+};
